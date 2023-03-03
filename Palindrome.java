b90c365a20bd44f9a1071c535696b69e13fe2d79
Abhimanyu-Modi <veerabhimanyu100@gmail.com>
2023-03-03 11:40:28 +0530

    Palindrome number

---
+++ Palindrome.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Palindrome
+{
+     public static void main(String[] args)
+     {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter number ");
+	int n=sc.nextInt();
+	boolean rs=isPalindrome(n);
+	if(rs)
+	   System.out.println(Palindrome");
+	else
+	   System.out.println("Not Palindrome");
+     }
+}
